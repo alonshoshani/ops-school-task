a3f796623316d6fdd3516de24bf0504924ddd03d
Alon Shoshani <alon@oribi.io>
2017-10-24 22:07:50 +0300

    change deny to all and restart apache

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise3 here
+perl -pe 's{deny}{++$n == 2 ? "allow" : $&}ge' /etc/apache2/sites-available/default > /tmp/default.tmp; sudo cp /tmp/default.tmp /etc/apache2/sites-available/default
+sudo service apache2 restart
